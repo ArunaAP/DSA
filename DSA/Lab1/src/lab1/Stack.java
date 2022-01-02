4a9f271390582212685f9fa47fce2c4f9013f5b4
Aruna priyankara <90405801+ArunaAP@users.noreply.github.com>
2022-01-02 22:34:10 +0530

    3rd commit

--- DSA/Lab1/src/lab1/Stack.java
+++ DSA/Lab1/src/lab1/Stack.java
@@ -5,21 +5,22 @@ public class Stack {
 	private char[] stackArray;
 	private int maxSize;
 	
-	public Stack(int s) {
+	public Stack(int maxS) {
 		this.top = -1;
-		this.stackArray = new char[s];
-		this.maxSize = s;
+		this.stackArray = new char[maxS];
+		this.maxSize = maxS;
 	}
 	
 	//To check stack is empty
 	public boolean isEmpty() {
-		return top == -1;
+		return (top == -1);
 	}
 	
 	public boolean isFull() {
 		return (top == (maxSize -1));
 	}
 	
+	//Insert element to stack
 	public void push(char ch) {
 		if(!isFull()) {
 			top = top + 1;
@@ -30,9 +31,20 @@ public class Stack {
 		}
 	}
 	
+	//retrieve element for stack
 	public char pop() {
 		if (!isEmpty()) {
 			return stackArray[top--];
+		}else {
+			System.out.println("Stack is empty");
+		}
+		return 0;
+	}
+	
+	//Get top element of stack
+	public char peek() {
+		if(!isEmpty()) {
+			return stackArray[top];
 		}else {
 			System.out.println("Stack is empty");
 		}
